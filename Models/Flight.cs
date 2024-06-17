0b96a368b074f2f26d44ecac0254c320d4bc0a7c
thavao <othavioact@gmail.com>
2024-06-17 15:03:43 -0300

    add: Models criada

---
+++ Models/Flight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Flight
+    {
+        public string Id { get; set; }
+        public int Sales { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Models/Passenger.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class Passenger
+    {
+        public string CPF { get; set; }
+        public string Name { get; set; }
+        public DateOnly DtBirth { get; set; }
+        public bool Status { get; set; }
+    }
+}
