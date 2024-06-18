b29fde7e63766eeb9427867a67c4940bd586cf8d
Edenilson Junior <edenilsonju10@gmail.com>
2024-06-18 11:12:43 -0300

    feat: created repositories and services class library

---
+++ Repositories/SaleRepository.cs
@@ -0,0 +1,26 @@
+﻿using Models;
+
+namespace Repositories
+{
+    public class SaleRepository
+    {
+
+        public SaleRepository() { }
+
+        public List<Sale> GetSale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sale GetSale(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sale Post(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Services/SaleService.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using Repositories;
+
+namespace Services
+{
+    public class SaleService
+    {
+        private SaleRepository _saleRepository;
+
+        public SaleService()
+        {
+            _saleRepository = new();
+        }
+
+        public List<Sale> GetSale()
+        {
+            return _saleRepository.GetSale();
+        }
+
+        public Sale GetSale(int id)
+        {
+            return _saleRepository.GetSale(id);
+        }
+
+        public Sale Post(Sale sale)
+        {
+            return _saleRepository.Post(sale);
+        }
+
+    }
+}
