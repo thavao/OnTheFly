565e120aeb1c07172a167db10eb1bb282f7fdffd
Edenilson Garcia <110670578+edenilsonjunior@users.noreply.github.com>
2024-06-19 15:28:47 -0300

    feat: final implementation of queue message using rabbitMq (Reserve a sale)

--- ReserveConsumer/Program.cs
+++ ReserveConsumer/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Services;
+using System.Data;
+using System.Text;
+
+namespace ReserveConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string QUEUE_NAME = "salesReservation";
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+
+            var service = new SaleService();
+
+
+            channel.QueueDeclare(
+                queue: QUEUE_NAME,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+
+            while (true)
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+
+                    var message = Encoding.UTF8.GetString(body);
+
+                    var sale = JsonConvert.DeserializeObject<Sale>(message);
+
+                    // Do something with the sale
+                    sale = service.Post(sale);
+
+                    if (sale == null)
+                        Console.WriteLine("Houve um erro ao inserir a venda");
+                    else
+                        Console.WriteLine("Venda inserida com sucesso");
+
+                    // Acknowledge the message
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+
+                channel.BasicConsume(
+                        queue: QUEUE_NAME,
+                        autoAck: false,
+                        consumer: consumer
+                    );
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
